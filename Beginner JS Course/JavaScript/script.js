8bd3df4445f432c4b9c95f7fcfa4419ee5969a2e
Blaine Rice <61302345+SquatchieJones@users.noreply.github.com>
2020-07-02 22:44:40 -0500

    Update script.js

--- Beginner JS Course/JavaScript/script.js
+++ Beginner JS Course/JavaScript/script.js
@@ -28,6 +28,7 @@ if (fruit == "banana", "bananas", "Bananas", "Banana") {
   document.getElementById("textDisplay").innerHTML = "I love grapes!";
 }}*/
 
+//This chunk does work
 /*document.querySelector('button').onclick = function(){
   alert('Stop poking me!');
 }*/
@@ -41,7 +42,8 @@ function chat() {
     case "bananas":
     case "Banana":
     case "Bananas":
-      document.getElementById("textDisplay").innerHTML = "Bananas go great with strawberries!";
+      document.getElementById("textDisplay").innerHTML =
+        "Bananas go great with strawberries!";
       break;
     case "apple":
     case "apples":
@@ -55,9 +57,30 @@ function chat() {
     case "Oranges":
       document.getElementById("textDisplay").innerHTML = "Oranges are okay..";
       break;
+    case "grape":
+    case "grapes":
+    case "Grape":
+    case "Green grapes":
+    case "Green Grapes":
+      document.getElementById("textDisplay").innerHTML =
+        "Grapes are refreshing! Purple ones are my favorite.";
+      break;
     default:
       document.getElementById("textDisplay").innerHTML =
         "I like all other fruits";
       break;
+  }
+}
+
+function motorcycles() {
+  var answer = document.getElementById("motorcycle").value;
+  var textDisplay2 = document.getElementById('textDisplay2');
+  
+  if (answer === "yes") {
+   location.href = ("https://squatchiejones.github.io/");
+  } else if (answer === "no") {
+    document.getElementById("textDisplay2").innerHTML = "Aw! Okay then...";
+  } else{
+    document.getElementById("textDisplay2").innerHTML = "That's okay, give it some thought and come back :)";
   }
 };
